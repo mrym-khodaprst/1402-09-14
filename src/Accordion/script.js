3a7776d4892017394243281bad4d3dd91e94e345
maryam <maryamkhodaparast88@yahoo.com>
2023-12-05 18:05:31 +0330

    modify accordion

--- src/Accordion/script.js
+++ src/Accordion/script.js
@@ -1,13 +1,13 @@
 let acc = document.getElementsByClassName("section-button");
 for(var i=0;i<acc.length;i++) {
     acc[i].addEventListener("click",function(){
-        this.classList.toggle("active");
+        this.classList.toggle("bg-slate-200");
         let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         }
         else{
-             panel.style.maxHeight = panel.scrollHeight + "px"; //nafahmidam
+             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
 }
