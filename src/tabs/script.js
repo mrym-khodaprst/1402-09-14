884d179ac899a1518c27130f4194a3ee60fd0f58
maryam <maryamkhodaparast88@yahoo.com>
2023-12-13 14:35:19 +0330

    optimize js and add home button in tabs

--- src/tabs/script.js
+++ src/tabs/script.js
@@ -1,13 +1,8 @@
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" bg-zinc-300", "");
-    }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " bg-zinc-300";
   }
